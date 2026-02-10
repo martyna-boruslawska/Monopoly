905e7db3d3dd7778df4e71aed6688dd67cf8da24
MartynaB <martyna.boruslawska@gmail.com>
2026-02-10 09:30:23 +0100

    added roll dice function

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 import { createPlayers } from "./game/createPlayers.js";
+import { createBoard } from "./game/createBoard.js";
+
+const board = createBoard();
 
 let players = createPlayers(["Martyna", "Jarek"]);
 players[1].money=150;
@@ -6,3 +9,10 @@ players[1].position=12;
 players[0].money+=200;
 players[0].position=10;
 console.log(players);
+
+function rollDice() {
+  const die1 = Math.floor(Math.random() * 6) + 1;
+  const die2 = Math.floor(Math.random() * 6) + 1;
+  return { total: die1 + die2, isDouble: die1 === die2 };
+}
+console.log("Rolled dice:", rollDice());

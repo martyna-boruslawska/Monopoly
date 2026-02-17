f249191394a627baac2c8c6a7189a8ce5145600a
MartynaB <martyna.boruslawska@gmail.com>
2026-02-17 13:41:01 +0100

    handled doubles

--- index.js
+++ index.js
@@ -8,6 +8,29 @@ const board = createBoard();
 let players = createPlayers(["Martyna", "Jarek"]);
 
 for (let turn = 0; turn < 10; turn++) {
-  movePlayer(players[0], rollDice().total, board);
-  movePlayer(players[1], rollDice().total, board);
+  // Player 0 turn
+  let doublesCount = 0;
+  let hasDouble = true;
+  while (hasDouble && doublesCount < 3) {
+    const roll = rollDice();
+    movePlayer(players[0], roll.total, board);
+    hasDouble = roll.isDouble;
+    if (hasDouble) {
+      console.log(`${players[0].name} rolled doubles: ${roll.dice1} & ${roll.dice2} and gets another turn!`);
+      doublesCount++;
+    }
+  }
+  
+  // Player 1 turn
+  doublesCount = 0;
+  hasDouble = true;
+  while (hasDouble && doublesCount < 3) {
+    const roll = rollDice();
+    movePlayer(players[1], roll.total, board);
+    hasDouble = roll.isDouble;
+    if (hasDouble) {
+      console.log(`${players[1].name} rolled doubles: ${roll.dice1} & ${roll.dice2} and gets another turn!`);
+      doublesCount++;
+    }
+  }
 }

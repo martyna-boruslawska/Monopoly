dfa54b71dad0b559cd0e0d67c69d6d8b500a5783
MartynaB <martyna.boruslawska@gmail.com>
2026-02-19 11:23:45 +0100

    separated playTheTurns

--- index.js
+++ index.js
@@ -1,36 +1,9 @@
 import { createPlayers } from "./game/createPlayers.js";
 import { createBoard } from "./game/createBoard.js";
-import { rollDice } from "./game/rollDice.js";
-import { movePlayer } from "./game/movePlayer.js";
+import { playTheTurns } from "./game/playTheTurns.js";
 
 const board = createBoard();
 
 let players = createPlayers(["Martyna", "Jarek"]);
 
-for (let turn = 0; turn < 10; turn++) {
-  // Player 0 turn
-  let doublesCount = 0;
-  let hasDouble = true;
-  while (hasDouble && doublesCount < 3) {
-    const roll = rollDice();
-    movePlayer(players[0], roll.total, board);
-    hasDouble = roll.isDouble;
-    if (hasDouble) {
-      console.log(`${players[0].name} rolled doubles: ${roll.dice1} & ${roll.dice2} and gets another turn!`);
-      doublesCount++;
-    }
-  }
-  
-  // Player 1 turn
-  doublesCount = 0;
-  hasDouble = true;
-  while (hasDouble && doublesCount < 3) {
-    const roll = rollDice();
-    movePlayer(players[1], roll.total, board);
-    hasDouble = roll.isDouble;
-    if (hasDouble) {
-      console.log(`${players[1].name} rolled doubles: ${roll.dice1} & ${roll.dice2} and gets another turn!`);
-      doublesCount++;
-    }
-  }
-}
+let theGame = playTheTurns(players, board, 10);

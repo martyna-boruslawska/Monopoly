3a952e0154f25ff7b74423c0cfd9a96741a642f0
MartynaB <martyna.boruslawska@gmail.com>
2026-02-19 14:43:56 +0100

    Homework: Game Start #1

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ import { playTheTurns } from "./game/playTheTurns.js";
 
 const board = createBoard();
 
-let players = createPlayers(["Martyna", "Jarek"]);
+let players = createPlayers(["Martyna Boruslawska", "Jarek Obrzut"]);
+
+console.log(`Players in the game: ${players.map(player => player.name).join(", ")}\n
+=================================\n
+🎲 MONOPOLY GAME STARTED! 🎲\n
+=================================\n\n`);
 
 let theGame = playTheTurns(players, board, 10); // Set how many turns to play (e.g., 10 turns)
